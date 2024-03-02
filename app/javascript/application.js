355fbc3f28ffdefb02694c2287254ae754685921
izebiafe <afimiafidelis95@gmail.com>
2024-03-02 15:56:10 +0100

    add models

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,23 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './redux/store';
+import ShowGreeting from './components/Greeting';
+import { StrictMode } from "react";
 
 function App() {
-  return (<h1>Hello World!</h1>);
-  }
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+     return (
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<ShowGreeting />} />
+            </Routes>
+        </BrowserRouter>
+    </Provider>
+  )
+}
+ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById("root"));
